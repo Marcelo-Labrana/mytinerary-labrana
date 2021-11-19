6adb5e649f98761040c211f036f3adddeec8af2e
luckycloown <sfmnll97@gmail.com>
2021-11-18 23:41:27 -0300

    del bootstrap

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,32 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function NavBar(){
     return (
         <div>
-            <a href ="https://www.google.com/">Link</a>
-            <a href ="https://www.google.com/">Link2</a>
-            <a href ="https://www.google.com/">Link3</a>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
         </div>
-        );
+    );
 }
 
 export default NavBar
