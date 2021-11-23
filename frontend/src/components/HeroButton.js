c7299193581c591ad207800937a1ceda0c4b3d8f
luckycloown <sfmnll97@gmail.com>
2021-11-23 09:22:43 -0300

    Ahora sí

--- frontend/src/components/HeroButton.js
+++ frontend/src/components/HeroButton.js
@@ -1,34 +1,34 @@
 import Reveal from 'react-reveal/Reveal';
 import Pulse from 'react-reveal/Pulse';
-import {Link} from 'react-router-dom';
-    
+import { Link } from 'react-router-dom';
+
 
 export default function HeroButton() {
-    
-    
-    
-        
 
-        return (
-            <div className="tittle-container justify-self-end">
-                <Reveal bottom>
+
+
+
+
+    return (
+        <div className="tittle-container justify-self-end">
+            <Reveal bottom>
+                <Link to="/cities">
                     <button className="hero-button">
-                        <Link to="/cities">
                         <div className="button-items">
-                            <img src="./assets/take-off.png"  alt="airplane taking off" className="plane"/>                            
+                            <img src="./assets/take-off.png" alt="airplane taking off" className="plane" />
                             <div className="button-text">
                                 <Pulse duration={3000} delay={6600} forever>
                                     <p className="button-text-1">Click here to</p>
                                     <p className="button-text-2">Start   your   Journey</p>
-                                </Pulse>    
+                                </Pulse>
                             </div>
-                            
+
                         </div>
-                        </Link> 
                     </button>
-                </Reveal>
-            </div>
-        );
-    
+                </Link>
+            </Reveal>
+        </div>
+    );
+
 }
 
